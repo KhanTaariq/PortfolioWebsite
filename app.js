08fb6838907403b3ab613c743ca23c46aa7655af
TaariqKhan <taariqkhs@gmail.com>
2022-04-28 02:13:55 +0200

    second commit

--- app.js
+++ app.js
@@ -28,8 +28,27 @@
     )
 }
 )();
-function myFunction()
-{
-    
-    alert("Message Recieved");  
-}
+
+src="https://smtpjs.com/v3/smtp.js"
+var btn = document.getElementById('btn')
+btn.addEventListener('click', function(e)
+{//console.log('hi');
+    e.preventDefault() 
+    var name = documnet.getElementById('name').value;
+    var email = documnet.getElementById('email').value;
+    var subject = documnet.getElementById('subject').value;
+    var message = documnet.getElementById('message').value;
+    var body = "name:" + name + "<br/> email: " + email + "<br/> subject: " + subject + "<br/> message: " + message;
+
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "taariqkhs@gmail.com",
+        Password : "aexoenndwbpfxgpq",
+        To : 'taariqkhs@gmail.com',
+        From : email,
+        Subject : subject,
+        Body : body
+    }).then(
+    message => alert("message sent successfully")
+    );  
+})
